e201bfa64b47ebd69366cc48f2907d20a984d686
Lama Radwan <lama.radwan96@gmail.com>
2022-07-05 19:30:32 +0300

    Added breadth first function

--- trees/trees.py
+++ trees/trees.py
@@ -91,6 +91,21 @@ class BinaryTree:
         else:
             return max(values)
 
+    def breadth_first(self):
+        if not self.root:
+            return self.root
+        output = []
+        queue = Queue()
+        queue.enqueue(self.root)
+        while not queue.isEmpty():
+            front = queue.dequeue()
+            output.append(front.value)
+            if front.left:
+                queue.enqueue(front.left)
+            if front.right:
+                queue.enqueue(front.right)
+        return output
+
 
 class BinarySearchTree(BinaryTree):
 
@@ -164,6 +179,7 @@ if __name__ == "__main__":
     print("++++++++ post order +++++++++")
     print(tree.post_order())
     print(tree.find_maximum())
+
 
     # bTree = BinarySearchTree()
     # bTree.add(23)
